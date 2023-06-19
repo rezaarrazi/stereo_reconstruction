5e31f8080b6e80b3696d6ab11cdb3cad79738ee4
CornellLenard <lenardzhang@foxmail.com>
2023-06-19 13:11:17 +0200

    updated test code

--- src/test.cpp
+++ src/test.cpp
@@ -1,18 +1,31 @@
 #include "stereo_dataset.h"
+#include "feature_extractor.h"
+#include "sparse_matcher.h"
 
 
 int main()
 {
+
     StereoDataset stereo_dataset;
     // stereo_dataset.TestDataset();
-    std::vector<std::string> image_pairs = stereo_dataset.GetImagePair(0);
-    std::cout << image_pairs[0] << '\n';
-    std::cout << image_pairs[1] << '\n';
+    std::array<std::string, 2> image_pairs = stereo_dataset.GetImagePair(0);
+    // std::cout << image_pairs[0] << '\n';
+    // std::cout << image_pairs[1] << '\n';
 
-    cv::Mat left_image = cv::imread(image_pairs[0], CV_LOAD_IMAGE_COLOR);
-    cv::imshow("left_image", left_image);
-    cv::waitKey(0);
+    cv::Mat image0 = cv::imread(image_pairs[0], CV_LOAD_IMAGE_COLOR);
+    cv::Mat image1 = cv::imread(image_pairs[1], CV_LOAD_IMAGE_COLOR);
+
+    FeatureExtractor feature_extractor;
+    feature_extractor.SetImagePair(image0, image1);
+    feature_extractor.ExtractFeaturesORB();
+
+    SparseMatcher sparse_matcher;
+    sparse_matcher.MatchSparsely(feature_extractor.GetKeypoints(), feature_extractor.GetFeatures());
+
+    sparse_matcher.DisplayMatchings(image0, image1, feature_extractor.GetKeypoints());
+
     return 0;
+
 }
 
 
