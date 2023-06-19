2f493de2c39c3df7a4d853b70aad7e41eb6d7ef8
CornellLenard <lenardzhang@foxmail.com>
2023-06-19 13:09:54 +0200

    refined StereoDataset implementation

--- src/stereo_dataset.cpp
+++ src/stereo_dataset.cpp
@@ -3,6 +3,7 @@
 
 StereoDataset::StereoDataset()
 {
+
     DIR* dir_ptr = nullptr;
     struct dirent* dirent_ptr = nullptr;
 
@@ -21,45 +22,41 @@ StereoDataset::StereoDataset()
             continue;
         }
 
-        folder_names.push_back(std::string(folder_name, folder_name + strlen(folder_name)));
+        folder_names_.push_back(std::string(folder_name, folder_name + strlen(folder_name)));
+
         dirent_ptr = readdir(dir_ptr);
     }
 
-    std::sort(folder_names.begin(), folder_names.end());
+    std::sort(folder_names_.begin(), folder_names_.end());
 
-    image_pair_num = folder_names.size();
+    image_pair_num_ = folder_names_.size();
 
     closedir(dir_ptr);
     dir_ptr = nullptr;
     dirent_ptr = nullptr;
+
 }
 
 
-std::vector<std::string> StereoDataset::GetImagePair(int image_id) const
+std::array<std::string, 2> StereoDataset::GetImagePair(int image_id) const
 {
-    std::vector<std::string> image_pairs;
+    std::array<std::string, 2> image_pairs;
 
-    if (image_id < 0 || image_id >= image_pair_num)
+    if (image_id < 0 || image_id >= image_pair_num_)
     {
         std::cout << "image_id should be >= 0 and < image_pair_num.\n";
         return image_pairs;
     }
 
-    std::string image_pair_path = DATA_PATH + "/" + folder_names[image_id] + "/";
+    std::string image_pair_path = DATA_PATH + "/" + folder_names_[image_id] + "/";
 
-    image_pairs.push_back(image_pair_path + "im0.png");
-    image_pairs.push_back(image_pair_path + "im1.png");
+    image_pairs[0] = image_pair_path + "im0.png";
+    image_pairs[1] = image_pair_path + "im1.png";
 
     return image_pairs;
 }
 
 
-void StereoDataset::TestDataset()
-{
-    std::cout << image_pair_num << '\n';
-    for (std::size_t i = 0; i < image_pair_num; i++)
-        std::cout << folder_names[i] << '\n';
-}
 
 
 
