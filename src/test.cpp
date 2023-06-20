4b97a4e89b36e5c3318c137d1dfd4cc507a2bd47
CornellLenard <lenardzhang@foxmail.com>
2023-06-20 13:06:58 +0200

    updated test file

--- src/test.cpp
+++ src/test.cpp
@@ -1,28 +1,39 @@
 #include "stereo_dataset.h"
 #include "feature_extractor.h"
 #include "sparse_matcher.h"
+#include "camera_pose_estimator.h"
 
 
 int main()
 {
 
     StereoDataset stereo_dataset;
-    // stereo_dataset.TestDataset();
-    std::array<std::string, 2> image_pairs = stereo_dataset.GetImagePair(0);
-    // std::cout << image_pairs[0] << '\n';
-    // std::cout << image_pairs[1] << '\n';
-
-    cv::Mat image0 = cv::imread(image_pairs[0], CV_LOAD_IMAGE_COLOR);
-    cv::Mat image1 = cv::imread(image_pairs[1], CV_LOAD_IMAGE_COLOR);
+    stereo_dataset.SetImages(0);
 
     FeatureExtractor feature_extractor;
-    feature_extractor.SetImagePair(image0, image1);
+    feature_extractor.SetImages(stereo_dataset.GetImages());
+
     feature_extractor.ExtractFeaturesORB();
 
     SparseMatcher sparse_matcher;
+
     sparse_matcher.MatchSparsely(feature_extractor.GetKeypoints(), feature_extractor.GetFeatures());
 
-    sparse_matcher.DisplayMatchings(image0, image1, feature_extractor.GetKeypoints());
+    // sparse_matcher.DisplayMatchings(stereo_dataset.GetImages(), feature_extractor.GetKeypoints());
+
+    stereo_dataset.SetCalibrations(0);
+
+    CameraPoseEstimator camera_pose_estimator;
+    camera_pose_estimator.SetCameraIntrinsics(stereo_dataset.GetCameraIntrinsics());
+    camera_pose_estimator.SetMatchedPoints(sparse_matcher.GetMatchedPoints());
+
+    camera_pose_estimator.EstimateCameraPose();
+
+    cv::Mat rotation = camera_pose_estimator.GetRotation();
+    cv::Mat translation = camera_pose_estimator.GetTranslation();
+
+    std::cout << rotation << '\n';
+    std::cout << translation << '\n';
 
     return 0;
 
