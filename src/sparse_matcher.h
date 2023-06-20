5c30c1de407592dcb9d931b61a242dd536ff3702
CornellLenard <lenardzhang@foxmail.com>
2023-06-20 13:06:37 +0200

    modified methods

--- src/sparse_matcher.h
+++ src/sparse_matcher.h
@@ -18,19 +18,20 @@ class SparseMatcher
         std::size_t match_num_ = 300;
         cv::Ptr<cv::DescriptorMatcher> feature_matcher_;
         std::vector<cv::DMatch> matches_;
-        std::vector<cv::Point2f> matched_points0_;
-        std::vector<cv::Point2f> matched_points1_;
+        std::array<std::vector<cv::Point2f>, 2> matched_points_;
         std::vector<cv::DMatch> selected_matches_;
         static bool CompareMatches(const cv::DMatch& match0, const cv::DMatch& match1);
 
     public:
         SparseMatcher();
 
+        std::array<std::vector<cv::Point2f>, 2> GetMatchedPoints() const;
+
         void SetMatchNum(std::size_t match_num);
 
         void MatchSparsely(const std::array<std::vector<cv::KeyPoint>, 2>& keypoints, const std::array<cv::Mat, 2>& features);
 
-        void DisplayMatchings(const cv::Mat& image0, const cv::Mat& image1, const std::array<std::vector<cv::KeyPoint>, 2>& keypoints);
+        void DisplayMatchings(const std::array<cv::Mat, 2>& images, const std::array<std::vector<cv::KeyPoint>, 2>& keypoints);
 
 };
 
