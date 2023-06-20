0534c4372c5ea0f47fb573a1acf30d374bd9f56b
CornellLenard <lenardzhang@foxmail.com>
2023-06-20 13:03:58 +0200

    added the header file for CameraPoseEstimator

---
+++ src/camera_pose_estimator.h
@@ -0,0 +1,45 @@
+#ifndef _CAMERA_POSE_ESTIMATOR
+#define _CAMERA_POSE_ESTIMATOR
+
+
+#include <array>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs/legacy/constants_c.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+
+
+class CameraPoseEstimator
+{
+
+    private:
+        std::array<cv::Mat, 2> camera_intrinsics_;
+        std::array<std::vector<cv::Point2f>, 2> matched_points_;
+        cv::Mat essential_matrix_;
+        cv::Mat rotation_;
+        cv::Mat translation_;
+
+    public:
+        CameraPoseEstimator() {}
+
+        cv::Mat GetRotation() const;
+
+        cv::Mat GetTranslation() const;
+
+        void SetCameraIntrinsics(const std::array<cv::Mat, 2>& camera_intrinsics);
+
+        void SetMatchedPoints(const std::array<std::vector<cv::Point2f>, 2>& matched_points);
+
+        void EstimateCameraPose();
+
+};
+
+
+#endif
+
+
+
+
+
+
