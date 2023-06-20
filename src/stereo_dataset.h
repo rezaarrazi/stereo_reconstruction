95ee85d6d15d8efbdfd21471b3656a9533cddc21
CornellLenard <lenardzhang@foxmail.com>
2023-06-20 13:03:18 +0200

    added members and methods, refactored

--- src/stereo_dataset.h
+++ src/stereo_dataset.h
@@ -4,6 +4,7 @@
 
 #include <dirent.h>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <array>
@@ -21,11 +22,30 @@ class StereoDataset
         const std::string DATA_PATH = "../Data/Middlebury";
         std::vector<std::string> folder_names_;
         std::size_t image_pair_num_ = 0;
+        std::array<cv::Mat, 2> images_;
+        std::array<cv::Mat, 2> disparities_;
+        std::array<cv::Mat, 2> camera_intrinsics_ = {cv::Mat_<float>(3, 3, 0.0), cv::Mat_<float>(3, 3, 0.0)};
+        float doffs_ = 0.0;
+        float baseline_ = 0.0;
+        std::size_t image_width_ = 0;
+        std::size_t image_height_ = 0;
+        std::size_t disparity_num_ = 0;
+        std::size_t min_disparity_ = 0;
+        std::size_t max_disparity_ = 0;
+        void ReadIntrinsics(const std::string& line, int camera_index);
 
     public:
         StereoDataset();
 
-        std::array<std::string, 2> GetImagePair(int image_id) const;
+        std::array<cv::Mat, 2> GetImages() const;
+
+        std::array<cv::Mat, 2> GetCameraIntrinsics() const;
+
+        void SetImages(int image_id);
+
+        void SetDisparities(int image_id);
+
+        void SetCalibrations(int image_id);
 
 };
 
