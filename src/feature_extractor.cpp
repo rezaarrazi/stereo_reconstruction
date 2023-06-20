7c378294e83e78c9c4140759f8b4b637c1610022
CornellLenard <lenardzhang@foxmail.com>
2023-06-20 13:06:09 +0200

    added implementations, refactored

--- src/feature_extractor.cpp
+++ src/feature_extractor.cpp
@@ -13,18 +13,17 @@ const std::array<cv::Mat, 2>& FeatureExtractor::GetFeatures() const
 }
 
 
-void FeatureExtractor::SetImagePair(const cv::Mat& image0, const cv::Mat& image1)
+void FeatureExtractor::SetImages(const std::array<cv::Mat, 2>& images)
 {
-    image0_ = image0;
-    image1_ = image1;
+    images_ = images;
 }
 
 
 void FeatureExtractor::ExtractFeaturesORB()
 {
     cv::Ptr<cv::ORB> orb_extractor = cv::ORB::create();
-    orb_extractor->detectAndCompute(image0_, cv::noArray(), keypoints_[0], features_[0]);
-    orb_extractor->detectAndCompute(image1_, cv::noArray(), keypoints_[1], features_[1]);
+    orb_extractor->detectAndCompute(images_[0], cv::noArray(), keypoints_[0], features_[0]);
+    orb_extractor->detectAndCompute(images_[1], cv::noArray(), keypoints_[1], features_[1]);
     features_[0].convertTo(features_[0], CV_32F);
     features_[1].convertTo(features_[1], CV_32F);
 }
