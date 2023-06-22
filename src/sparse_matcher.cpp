02fdc2141c44b46bc087b617669f76a5500e7e3c
CornellLenard <lenardzhang@foxmail.com>
2023-06-22 05:22:12 +0200

    added more implementations

--- src/sparse_matcher.cpp
+++ src/sparse_matcher.cpp
@@ -9,7 +9,8 @@ bool SparseMatcher::CompareMatches(const cv::DMatch& match0, const cv::DMatch& m
 
 SparseMatcher::SparseMatcher()
 {
-    feature_matcher_ = cv::DescriptorMatcher::create("BruteForce");
+    bf_matcher_ = cv::DescriptorMatcher::create("BruteForce");
+    flann_based_matcher_ = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
 }
 
 
@@ -19,22 +20,17 @@ std::array<std::vector<cv::Point2f>, 2> SparseMatcher::GetMatchedPoints() const
 }
 
 
-void SparseMatcher::SetMatchNum(std::size_t match_num)
-{
-    match_num_ = match_num;
-}
-
-
-void SparseMatcher::MatchSparsely(const std::array<std::vector<cv::KeyPoint>, 2>& keypoints, const std::array<cv::Mat, 2>& features)
+void SparseMatcher::MatchSparselyBFSortTop(const std::array<std::vector<cv::KeyPoint>, 2>& keypoints,
+                                           const std::array<cv::Mat, 2>& features, std::size_t keypoint_number)
 {
 
-    feature_matcher_->match(features[0], features[1], matches_);
+    bf_matcher_->match(features[0], features[1], matches_);
     std::sort(matches_.begin(), matches_.end(), CompareMatches);
 
     cv::KeyPoint keypoint;
     cv::Point2f point;
 
-    for (std::size_t i = 0; i < match_num_; i++)
+    for (std::size_t i = 0; i < keypoint_number; i++)
     {
         keypoint = keypoints[0][matches_[i].queryIdx];
         matched_points_[0].push_back(cv::Point2f(keypoint.pt.x, keypoint.pt.y));
@@ -43,6 +39,60 @@ void SparseMatcher::MatchSparsely(const std::array<std::vector<cv::KeyPoint>, 2>
         matched_points_[1].push_back(cv::Point2f(keypoint.pt.x, keypoint.pt.y));
 
         selected_matches_.push_back(matches_[i]);
+    }
+
+}
+
+
+void SparseMatcher::MatchSparselyBFMinDistance(const std::array<std::vector<cv::KeyPoint>, 2>& keypoints,
+                                               const std::array<cv::Mat, 2>& features, float distance_ratio)
+{
+
+    bf_matcher_->match(features[0], features[1], matches_);
+    std::sort(matches_.begin(), matches_.end(), CompareMatches);
+
+    cv::KeyPoint keypoint;
+    cv::Point2f point;
+
+    for (std::size_t i = 0; i < matches_.size(); i++)
+    {
+        if (matches_[i].distance >= distance_ratio * matches_[0].distance)
+            break;
+
+        keypoint = keypoints[0][matches_[i].queryIdx];
+        matched_points_[0].push_back(cv::Point2f(keypoint.pt.x, keypoint.pt.y));
+
+        keypoint = keypoints[1][matches_[i].trainIdx];
+        matched_points_[1].push_back(cv::Point2f(keypoint.pt.x, keypoint.pt.y));
+
+        selected_matches_.push_back(matches_[i]);
+    }
+
+}
+
+
+void SparseMatcher::MatchSparselyFLANNBased(const std::array<std::vector<cv::KeyPoint>, 2>& keypoints, const std::array<cv::Mat, 2>& features, float ratio)
+{
+
+    std::vector<std::vector<cv::DMatch>> flann_based_matches;
+    flann_based_matcher_->knnMatch(features[0], features[1], flann_based_matches, 2);
+
+    cv::KeyPoint keypoint;
+    cv::Point2f point;
+
+    for (std::size_t i = 0; i < flann_based_matches.size(); i++)
+    {
+        if (flann_based_matches[i][0].distance >= flann_based_matches[i][1].distance * ratio)
+            continue;
+
+        keypoint = keypoints[0][flann_based_matches[i][0].queryIdx];
+        matched_points_[0].push_back(cv::Point2f(keypoint.pt.x, keypoint.pt.y));
+
+        keypoint = keypoints[1][flann_based_matches[i][0].trainIdx];
+        matched_points_[1].push_back(cv::Point2f(keypoint.pt.x, keypoint.pt.y));
+
+        selected_matches_.push_back(flann_based_matches[i][0]);
+        
     }
 
 }
