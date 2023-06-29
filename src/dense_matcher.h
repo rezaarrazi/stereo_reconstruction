cc6569ca03e842f42d0be07d6373cf79e8dea201
CornellLenard <lenardzhang@foxmail.com>
2023-06-29 23:46:53 +0200

    refactored the names of constant members

--- src/dense_matcher.h
+++ src/dense_matcher.h
@@ -14,19 +14,21 @@ class DenseMatcher
 {
 
     private:
-        int bm_disparity_number_ = 256;
-        int bm_block_size_ = 21;
-        int sgbm_min_disparity_ = 0;
-        int sgbm_disparity_number_ = 256;
-        int sgbm_block_size_ = 5;
-        int sgbm_disp12_max_diff_ = -1;
-        int sgbm_speckle_window_size_ = 100;
-        int sgbm_speckle_range_ = 32;
-        int pre_filter_cap_ = 63;
-        int uniqueness_ratio_ = 5;
+        int BM_DISPARITY_NUMBER_ = 256;
+        int BM_BLOCK_SIZE_ = 21;
+        int SGBM_MIN_DISPARITY_ = 0;
+        int SGBM_DISPARITY_NUMBER_ = 256;
+        int SGBM_BLOCK_SIZE_ = 5;
+        int SGBM_DISP12_MAX_DIFF_ = -1;
+        int SGBM_SPECKLE_WINDOW_SIZE_ = 100;
+        int SGBM_SPECKLE_RANGE_ = 32;
+        int PRE_FILTER_CAP_ = 63;
+        int UNIQUENESS_RATIO_ = 5;
+
         std::array<cv::Mat, 2> rectified_images_;
         cv::Mat disparity_map_;
         cv::Mat colorful_disparity_map_;
+
         void FillHolesMaximum(const StereoDataset& stereo_dataset, std::size_t window_size);
         void FillHolesAverage(const StereoDataset& stereo_dataset, std::size_t window_size);
 
