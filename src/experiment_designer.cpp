2137cd8758325f933c24b026ac1e32fce24ad719
CornellLenard <lenardzhang@foxmail.com>
2023-06-29 22:01:14 +0200

    refactored, added implementation of CompareCameraPoseEstimation()

--- src/experiment_designer.cpp
+++ src/experiment_designer.cpp
@@ -50,7 +50,7 @@ void ExperimentDesigner::CompareKeypointNumber()
 }
 
 
-void ExperimentDesigner::CompareCameraPoseEstimation(std::size_t type, std::size_t keypoint_number, double distance_ratio, double ratio)
+void ExperimentDesigner::CompareFeatureExtractionAndMatching(std::size_t type, std::size_t keypoint_number, double distance_ratio, double ratio)
 {
 
     // The order is ORB, SIFT, SURF, BRISK
@@ -83,22 +83,12 @@ void ExperimentDesigner::CompareCameraPoseEstimation(std::size_t type, std::size
                 sparse_matcher_.MatchSparselyFLANNBased(feature_extractor_.GetKeypoints(), feature_extractor_.GetFeatures(), ratio);
             
             camera_pose_estimator_.SetMatchedPoints(sparse_matcher_.GetMatchedPoints());
-            camera_pose_estimator_.EstimateCameraPose();
+            camera_pose_estimator_.EstimateCameraPose(3);
 
             std::array<double, 2> rmse = ComputeRMSE(camera_pose_estimator_.GetRotation(), camera_pose_estimator_.GetTranslation());
 
             rmses[j][0] += rmse[0];
             rmses[j][1] += rmse[1];
-
-            // cv::Mat rotation = camera_pose_estimator_.GetRotation();
-            // cv::Mat translation = camera_pose_estimator_.GetTranslation();
-
-            // std::cout << rotation.type() << '\n';
-
-            // std::cout << rotation << '\n';
-            // std::cout << translation << '\n';
-
-            // std::cout << rmses[j][0] << "   " << rmses[j][1] << "\n\n";
         }
 
     }
@@ -114,7 +104,53 @@ void ExperimentDesigner::CompareCameraPoseEstimation(std::size_t type, std::size
     for (std::size_t i = 0; i < 4; i++)
         std::cout << feature_extractor_names_[i] << ": rotation: " << rmses[i][0] << " translation: " << rmses[i][1] << '\n';
 
+}
 
+
+void ExperimentDesigner::CompareCameraPoseEstimation()
+{
+
+    std::array<std::array<double, 2>, 4> rmses;
+
+    for (std::size_t i = 0; i < 4; i++)
+        for (std::size_t j = 0; j < 2; j++)
+            rmses[i][j] = 0.0;
+    
+    std::size_t image_pair_number = stereo_dataset_.GetImagePairNumber();
+
+    for (std::size_t i = 0; i < image_pair_number; i++)
+    {
+        stereo_dataset_.SetImages(i);
+        stereo_dataset_.SetCalibrations(i);
+
+        feature_extractor_.SetImages(stereo_dataset_.GetImages());
+
+        camera_pose_estimator_.SetCameraIntrinsics(stereo_dataset_.GetCameraIntrinsics());
+
+        feature_extractor_.ExtractFeatures(3);
+        sparse_matcher_.MatchSparselyFLANNBased(feature_extractor_.GetKeypoints(), feature_extractor_.GetFeatures(), 0.6);
+
+        camera_pose_estimator_.SetMatchedPoints(sparse_matcher_.GetMatchedPoints());
+
+        for (std::size_t j = 0; j < 4; j++)
+        {
+            camera_pose_estimator_.EstimateCameraPose(j);
+
+            std::array<double, 2> rmse = ComputeRMSE(camera_pose_estimator_.GetRotation(), camera_pose_estimator_.GetTranslation());
+
+            rmses[j][0] += rmse[0];
+            rmses[j][1] += rmse[1];
+        }
+    }
+
+    for (std::size_t i = 0; i < 4; i++)
+    {
+        rmses[i][0] /= image_pair_number;
+        rmses[i][1] /= image_pair_number;
+    }
+
+    for (std::size_t i = 0; i < 4; i++)
+        std::cout << camera_pose_estimator_names_[i] << ": rotation: " << rmses[i][0] << " translation: " << rmses[i][1] << '\n';
 
 }
 
