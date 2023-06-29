747d659f8b3bbe0eec8f503084f80e177a0c7fa3
CornellLenard <lenardzhang@foxmail.com>
2023-06-30 00:24:59 +0200

    updated, added implementation of CompareDisparrityMaps()

--- src/experiment_designer.cpp
+++ src/experiment_designer.cpp
@@ -8,13 +8,13 @@ std::array<double, 2> ExperimentDesigner::ComputeRMSE(const cv::Mat& rotation, c
 
     for (std::size_t i = 0; i < 3; i++)
         for (std::size_t j = 0; j < 3; j++)
-            rmses[0] += pow(rotation.at<double>(i, j) - rotation_gt_.at<double>(i, j), 2);
+            rmses[0] += pow(rotation.at<double>(i, j) - ROTATION_GT_.at<double>(i, j), 2);
 
     rmses[0] /= 9.0;
     rmses[0] = sqrt(rmses[0]);
 
     for (std::size_t i = 0; i < 3; i++)
-        rmses[1] += pow(translation.at<double>(i, 0) - translation_gt_.at<double>(i, 0), 2);
+        rmses[1] += pow(translation.at<double>(i, 0) - TRANSLATION_GT_.at<double>(i, 0), 2);
 
     rmses[1] /= 3.0;
     rmses[1] = sqrt(rmses[1]);
@@ -45,7 +45,7 @@ void ExperimentDesigner::CompareKeypointNumber()
     }
 
     for (std::size_t i = 0; i < 4; i++)
-        std::cout << feature_extractor_names_[i] << ": " << static_cast<std::size_t>(keypoint_numbers[i] / image_pair_number) << '\n';
+        std::cout << FEATURE_EXTRACTOR_NAMES_[i] << ": " << static_cast<std::size_t>(keypoint_numbers[i] / image_pair_number) << '\n';
 
 }
 
@@ -99,10 +99,10 @@ void ExperimentDesigner::CompareFeatureExtractionAndMatching(std::size_t type, s
         rmses[i][1] /= image_pair_number;
     }
 
-    std::cout << sparse_matcher_names_[type] << '\n';
+    std::cout << SPARSE_MATCHER_NAMES_[type] << '\n';
 
     for (std::size_t i = 0; i < 4; i++)
-        std::cout << feature_extractor_names_[i] << ": rotation: " << rmses[i][0] << " translation: " << rmses[i][1] << '\n';
+        std::cout << FEATURE_EXTRACTOR_NAMES_[i] << ": rotation: " << rmses[i][0] << " translation: " << rmses[i][1] << '\n';
 
 }
 
@@ -150,7 +150,57 @@ void ExperimentDesigner::CompareCameraPoseEstimation()
     }
 
     for (std::size_t i = 0; i < 4; i++)
-        std::cout << camera_pose_estimator_names_[i] << ": rotation: " << rmses[i][0] << " translation: " << rmses[i][1] << '\n';
+        std::cout << CAMERA_POSE_ESTIMATOR_NAMES_[i] << ": rotation: " << rmses[i][0] << " translation: " << rmses[i][1] << '\n';
+
+}
+
+
+void ExperimentDesigner::CompareDisparityMaps()
+{
+
+    stereo_dataset_.SetImages(0);
+    stereo_dataset_.SetCalibrations(0);
+    stereo_dataset_.SetDisparityMaps(0);
+
+    feature_extractor_.SetImages(stereo_dataset_.GetImages());
+
+    std::array<std::size_t, 2> feature_extractor_types = {1, 3};
+    std::array<std::string, 2> feature_extractor_names = {"SIFT", "BRISK"};
+    std::array<std::string, 2> dense_matcher_names = {"StereoBM", "StereoSGBM"};
+
+    cv::Mat rotation;
+    cv::Mat translation;
+
+    for (std::size_t i = 0; i < 2; i++)
+    {
+        feature_extractor_.ExtractFeatures(feature_extractor_types[i]);
+
+        if (feature_extractor_types[i] == 1)
+            sparse_matcher_.MatchSparselyBFSortTop(feature_extractor_.GetKeypoints(), feature_extractor_.GetFeatures(), 50);
+        else
+            sparse_matcher_.MatchSparselyFLANNBased(feature_extractor_.GetKeypoints(), feature_extractor_.GetFeatures(), 0.6);
+        
+        camera_pose_estimator_.SetCameraIntrinsics(stereo_dataset_.GetCameraIntrinsics());
+
+        camera_pose_estimator_.SetMatchedPoints(sparse_matcher_.GetMatchedPoints());
+
+        camera_pose_estimator_.EstimateCameraPose(3);
+
+        rotation = camera_pose_estimator_.GetRotation();
+        translation = camera_pose_estimator_.GetTranslation();
+
+        dense_matcher_.RectifyImages(stereo_dataset_, rotation, translation);
+
+        for (std::size_t j = 0; j < 2; j++)
+        {
+            dense_matcher_.ComputeDisparityMap(stereo_dataset_, rotation, translation, j);
+            cv::imwrite("../" + feature_extractor_names[i] + dense_matcher_names[j] + ".png", dense_matcher_.GetColorfulDisparityMap());
+        }
+    }
+
+    cv::Mat gt;
+    cv::applyColorMap(stereo_dataset_.GetDisparityMaps()[0], gt, cv::COLORMAP_JET);
+    cv::imwrite("../gt.png", gt);
 
 }
 
