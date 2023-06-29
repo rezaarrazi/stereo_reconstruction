ff983f557cd7ac20ef3c7348f16ae09c3a03da3a
CornellLenard <lenardzhang@foxmail.com>
2023-06-29 21:59:35 +0200

    refactored EstimateCameraPose()

--- src/camera_pose_estimator.cpp
+++ src/camera_pose_estimator.cpp
@@ -25,11 +25,37 @@ void CameraPoseEstimator::SetMatchedPoints(const std::array<std::vector<cv::Poin
 }
 
 
-void CameraPoseEstimator::EstimateCameraPose()
+void CameraPoseEstimator::EstimateCameraPose(std::size_t type)
 {
-    cv::recoverPose(matched_points_[0], matched_points_[1],
-                    camera_intrinsics_[0], cv::noArray(), camera_intrinsics_[1], cv::noArray(),
-                    essential_matrix_, rotation_, translation_);
+
+    if (type == 0)
+    {
+        // Five-Point Algorithm
+        essential_matrix_ = cv::findEssentialMat(matched_points_[0], matched_points_[1], camera_intrinsics_[0], cv::noArray(),
+                                                 camera_intrinsics_[1], cv::noArray());
+        cv::recoverPose(essential_matrix_, matched_points_[0], matched_points_[1], rotation_, translation_);
+    }
+    else if (type == 1)
+    {
+        // Seven-Point Algorithm
+        cv::Mat fundamental_matrix = cv::findFundamentalMat(matched_points_[0], matched_points_[1], cv::FM_7POINT);
+        essential_matrix_ = camera_intrinsics_[1].t() * fundamental_matrix * camera_intrinsics_[0];
+        cv::recoverPose(essential_matrix_, matched_points_[0], matched_points_[1], rotation_, translation_);
+    }
+    else if (type == 2)
+    {
+        // Eight-Point Algorithm
+        cv::Mat fundamental_matrix = cv::findFundamentalMat(matched_points_[0], matched_points_[1], cv::FM_8POINT);
+        essential_matrix_ = camera_intrinsics_[1].t() * fundamental_matrix * camera_intrinsics_[0];
+        cv::recoverPose(essential_matrix_, matched_points_[0], matched_points_[1], rotation_, translation_);
+    }
+    else if (type == 3)
+        // Five-Point Algorithm
+        cv::recoverPose(matched_points_[0], matched_points_[1], camera_intrinsics_[0], cv::noArray(),
+                        camera_intrinsics_[1], cv::noArray(), essential_matrix_, rotation_, translation_);
+    else
+        std::cout << "type should be >= 0 and < 4\n";
+
 }
 
 
