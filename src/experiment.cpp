d7e0f532454634bd9520b21797cc5178a6f40bfc
CornellLenard <lenardzhang@foxmail.com>
2023-06-29 21:59:58 +0200

    updated

--- src/experiment.cpp
+++ src/experiment.cpp
@@ -8,11 +8,13 @@ int main()
 
     // experiment_designer.CompareKeypointNumber();
 
-    for (std::size_t i = 0; i < 3; i++)
-    {
-        experiment_designer.CompareCameraPoseEstimation(i);
-        std::cout << "\n\n";
-    }
+    // for (std::size_t i = 0; i < 3; i++)
+    // {
+    //     experiment_designer.CompareFeatureExtractionAndMatching(i);
+    //     std::cout << "\n\n";
+    // }
+
+    experiment_designer.CompareCameraPoseEstimation();
 
     return 0;
 
