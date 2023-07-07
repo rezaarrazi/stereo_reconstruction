447af5b17ecaffa7d9d2bebf944eabb608874967
CornellLenard <lenardzhang@foxmail.com>
2023-07-07 03:10:31 +0200

    added LoadData(), refactored parameters of some methods

--- src/dense_matcher.h
+++ src/dense_matcher.h
@@ -17,6 +17,7 @@ class DenseMatcher
     private:
         int BM_DISPARITY_NUMBER_ = 256;
         int BM_BLOCK_SIZE_ = 21;
+
         int SGBM_MIN_DISPARITY_ = 0;
         int SGBM_DISPARITY_NUMBER_ = 256;
         int SGBM_BLOCK_SIZE_ = 5;
@@ -26,6 +27,11 @@ class DenseMatcher
         int PRE_FILTER_CAP_ = 63;
         int UNIQUENESS_RATIO_ = 5;
 
+        std::array<cv::Mat, 2> images_;
+        cv::Size image_size_;
+        std::array<cv::Mat, 2> camera_intrinsics_;
+        cv::Mat rotation_;
+        cv::Mat translation_;
         std::array<cv::Mat, 2> rectified_images_;
         cv::Mat disparity_map_;
         cv::Mat colorful_disparity_map_;
@@ -35,13 +41,17 @@ class DenseMatcher
     public:
         DenseMatcher() {}
 
+        void LoadData(const StereoDataset& stereo_dataset, const cv::Mat& rotation, const cv::Mat& translation);
+
         cv::Mat GetDisparityMap() const;
 
         cv::Mat GetColorfulDisparityMap() const;
 
-        void RectifyImages(const StereoDataset& stereo_dataset, const cv::Mat& rotation, const cv::Mat& translation);
+        void RectifyImages();
 
-        void ComputeDisparityMap(const StereoDataset& stereo_dataset, const cv::Mat& rotation, const cv::Mat& translation, std::size_t type);
+        void ComputeDisparityMap(std::size_t type);
+
+        void ComputeDisparityMapDirectly(std::size_t type);
 
 };
 
