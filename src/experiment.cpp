491cd2d62d0b8466f7cd6f2ed338554a29861dc9
CornellLenard <lenardzhang@foxmail.com>
2023-07-07 20:54:09 +0200

    updated

--- src/experiment.cpp
+++ src/experiment.cpp
@@ -16,7 +16,16 @@ int main()
 
     // experiment_designer.CompareCameraPoseEstimation();
 
-    experiment_designer.CompareDisparityMaps();
+    // experiment_designer.PrintDisparityMaps(0);
+
+    // experiment_designer.CompareDisparityMaps(1, 0, 0);
+    // std::cout << "\n\n";
+    experiment_designer.CompareDisparityMaps(1, 0, 1);
+    std::cout << "\n\n";
+    // experiment_designer.CompareDisparityMaps(3, 2, 0);
+    // std::cout << "\n\n";
+    // experiment_designer.CompareDisparityMaps(3, 2, 1);
+    // std::cout << "\n\n";
 
     return 0;
 
