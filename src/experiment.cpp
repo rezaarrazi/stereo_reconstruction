09058aeb00b622393f4898a32a46f8210d42661f
CornellLenard <lenardzhang@foxmail.com>
2023-07-25 00:33:18 +0200

    updated experiment.cpp

--- src/experiment.cpp
+++ src/experiment.cpp
@@ -8,9 +8,21 @@ int main()
 
     // experiment_designer.CompareKeypointNumber();
 
-    // for (std::size_t i = 0; i < 3; i++)
+    // for (std::size_t i = 0; i < 4; i++)
     // {
-    //     experiment_designer.CompareFeatureExtractionAndMatching(i);
+    //     experiment_designer.CompareFeatureExtractionAndBFSortTop(i);
+    //     std::cout << "\n\n";
+    // }
+
+    // for (std::size_t i = 1; i < 4; i++)
+    // {
+    //     experiment_designer.CompareFeatureExtractionAndBFMinDistance(i);
+    //     std::cout << "\n\n";
+    // }
+
+    // for (std::size_t i = 1; i < 4; i++)
+    // {
+    //     experiment_designer.CompareFeatureExtractionAndFLANNBased(i);
     //     std::cout << "\n\n";
     // }
 
@@ -18,16 +30,12 @@ int main()
 
     // experiment_designer.PrintDisparityMaps(0);
 
-    // experiment_designer.CompareDisparityMaps(1, 0, 0);
-    // std::cout << "\n\n";
-    // experiment_designer.CompareDisparityMaps(1, 0, 1);
-    // std::cout << "\n\n";
-    // experiment_designer.CompareDisparityMaps(3, 2, 0);
-    // std::cout << "\n\n";
-    // experiment_designer.CompareDisparityMaps(3, 2, 1);
-    // std::cout << "\n\n";
+    experiment_designer.CompareDisparityMaps(0);
+    std::cout << "\n\n";
+    experiment_designer.CompareDisparityMaps(1);
+    std::cout << "\n\n";
 
-    experiment_designer.ReconstructScenesDirectly(0, 1);
+    // experiment_designer.ReconstructScenesDirectly(0, 1);
 
     return 0;
 
